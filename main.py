687a0ed51cd64c924f81edc5915fe679388d1334
mruczekpln <leonwdev@gmail.com>
2024-05-15 12:23:48 +0200

    google cloud app registration and quicktest using google's quickstart app

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import asyncio
-from vulcan import Vulcan, Keystore, Account
+from vulcan import Vulcan, Keystore , Account, data
+
 
 async def main():
   client = await load_client()
   await client.select_student()
+
+  lessons = await client.data.get_lessons()
+  async for lesson in lessons:
+    print (lesson)
+    # print(f"{changed_lesson.subject.name} w sali {changed_lesson.room.code} z {changed_lesson.teacher.name} {changed_lesson.teacher.surname}.")
+  
+  # changed_lessons = await client.data.get_changed_lessons()
+  # async for changed_lesson in changed_lessons:
+  #   print (changed_lesson)
+  #   # print(f"Zmieniona {changed_lesson.subject.name}, notka: {changed_lesson.note}")
+
+  # closing http session
+  await client.close()
 
 async def load_client():
   # loading keystore data
@@ -15,7 +29,6 @@ async def load_client():
     account = Account.load(f)
   
   return Vulcan(keystore, account)
-  
 
 if __name__ == "__main__":
   loop = asyncio.get_event_loop()
